ff4cb88d2bb75fcaef7897dfc5274a6155c527d4
Yago Silva and Yago Silva <login+Silva+Yago@elo7.com>
2015-03-14 19:08:17 -0300

    Refatora CandlestickFactory para passar no teste onde o cenário é uma lista vazia (essa refatoração causa um bug intencional)

--- src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
+++ src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class CandlestickFactory {
 	public Candlestick constroiCandleParaData(Calendar data, List<Negociacao> negociacoes){
-		double minimo = negociacoes.get(0).getPreco();
-		double maximo = negociacoes.get(0).getPreco();;
+		double minimo = Double.MAX_VALUE;
+		double maximo = 0;
 		double volume = 0;
 
 		for(Negociacao negociacao : negociacoes){
@@ -19,8 +19,8 @@ public class CandlestickFactory {
 			}
 		}
 
-		double abertura = negociacoes.get(0).getPreco();
-		double fechamento = negociacoes.get(negociacoes.size() - 1).getPreco();
+		double abertura = negociacoes.isEmpty() ? 0 : negociacoes.get(0).getPreco();
+		double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(negociacoes.size() - 1).getPreco();
 
 		return new Candlestick(abertura, fechamento, minimo, maximo, volume, data);
 	}
