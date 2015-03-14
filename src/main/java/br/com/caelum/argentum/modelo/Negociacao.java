ea17b0b155c325fe6e7861f1dcc3062b3f6c36e5
Yago Silva and Yago Silva <login+Silva+Yago@elo7.com>
2015-03-14 10:17:18 -0300

    Adiciona classe negociação

---
+++ src/main/java/br/com/caelum/argentum/modelo/Negociacao.java
@@ -0,0 +1,32 @@
+package br.com.caelum.argentum.modelo;
+
+import java.util.Calendar;
+
+public final class Negociacao {
+	private final double preco;
+	private final int quantidade;
+	private final Calendar data;
+
+	public Negociacao(double preco, int quantidade, Calendar data) {
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public double getVolume(){
+		return preco * quantidade;
+	}
+
+}
